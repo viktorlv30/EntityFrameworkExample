bd1a3ff97066e87bfd1ada07b935464f2b45d2bb
viktorlv30 <viktor.lv.30@gmail.com>
2016-07-06 22:21:24 +0300

    try to write "todo" comments

--- EFExample/EFExample/Program.cs
+++ EFExample/EFExample/Program.cs
@@ -6,10 +6,13 @@ using System.Threading.Tasks;
 
 namespace EFExample
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            //todo
+            //usual comment
+
         }
     }
 }
